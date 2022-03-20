fe37f58b8d94c5a52886418d7a7a47dfc856f1b4
AlvaroCarbajoAlcalde <alvarocarbajoalcalde@gmail.com>
2022-03-20 13:35:16 +0100

    tablet map route : )

--- BalloonSimFront/src/app/sim/sim.component.ts
+++ BalloonSimFront/src/app/sim/sim.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as L from 'leaflet';
-import { catchError } from 'rxjs';
+import { catchError, reduce } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import SkyboxController from '../class/skyboxController';
 import takeoffController from '../class/takeoffController';
@@ -24,10 +24,13 @@ declare let balloon: any;
 export class SimComponent implements OnInit, OnDestroy, AfterViewInit {
   private map!: L.Map;
   private mapUpdateInterval!: any;
+  private pointsSaveInterval!: any;
+  private pointList!: number[][];
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.pointList = [];
     document.body.classList.add('no-overflow');
     testing = environment.testing;
     showCollisions = environment.showCollisions;
@@ -54,38 +57,59 @@ export class SimComponent implements OnInit, OnDestroy, AfterViewInit {
     //#region map
     this.map = L.map('map', {
       center: [42.55878426869105, -2.8633044423426677],
-      zoom: 11,
+      zoom: 14,
     });
 
     const tiles = L.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
         maxZoom: 15,
-        minZoom: 12,
+        minZoom: 10,
       }
     );
 
     tiles.addTo(this.map);
     //#endregion
 
+    const route = new L.Polyline([], {
+      color: 'red',
+      weight: 3,
+      opacity: 0.7,
+      smoothFactor: 1,
+    });
+    route.addTo(this.map);
+
     const marker = L.circleMarker([0, 0], { radius: 2 });
     marker.addTo(this.map);
 
     this.mapUpdateInterval = setInterval(() => {
-      try {
-        marker.setLatLng(
-          new L.LatLng(balloon.calcDegreesLat(), balloon.calcDegreesLon())
+      if (balloon) {
+        const latLng = new L.LatLng(
+          balloon.calcDegreesLat(),
+          balloon.calcDegreesLon()
         );
-        this.map.panTo(
-          new L.LatLng(balloon.calcDegreesLat(), balloon.calcDegreesLon())
-        );
-      } catch (e) {}
+        marker.setLatLng(latLng);
+        this.map.panTo(latLng);
+        route.addLatLng(latLng);
+      }
     }, 250);
+
+    this.mapUpdateInterval = setInterval(() => {
+      if (balloon) {
+        this.pointList.push([
+          balloon.calcDegreesLat(),
+          balloon.calcDegreesLon(),
+        ]);
+      }
+    }, 3000);
   }
 
   ngOnDestroy(): void {
     document.body.classList.remove('no-overflow');
     clearInterval(this.mapUpdateInterval);
+    console.groupCollapsed('Ruta');
+    console.table(this.pointList);
+    console.groupEnd();
   }
 
   endGame() {
